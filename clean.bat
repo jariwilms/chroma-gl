5a726744eb5ec8c7fa18c1d92662f3fffbb97764
Jari <jariwilms@outlook.com>
2025-10-23 05:56:07 +0200

    bin/ and build/ removal

--- clean.bat
+++ clean.bat
@@ -1,6 +1,6 @@
 @echo off
 
-del /s /q "%~dp0*.sln"
-del /s /q "%~dp0*.vcxproj*"
-
-echo Removed all .sln and .vcxproj files recursively.
+rmdir /s /q "%~dp0bin"
+rmdir /s /q "%~dp0build"
+del   /s /q "%~dp0*.sln"
+del   /s /q "%~dp0*.vcxproj*"
