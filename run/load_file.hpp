5c02e8c4f0b78ad807d2330184954620afaa5a63
Jari <jariwilms@outlook.com>
2025-10-23 02:57:44 +0200

    triangle and texture examples

---
+++ run/load_file.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+import std;
+import chroma_gl;
+
+static auto load_file(std::filesystem::path const& filepath) -> std::vector<gl::byte_t>
+{
+    auto file = std::ifstream{ filepath, std::ios::binary | std::ios::ate };
+    if (!file) throw std::runtime_error{ "failed to open file: " + filepath.string() };
+
+    auto const size = file.tellg();
+    auto       buffer = std::vector<gl::byte_t>(static_cast<gl::size_t>(size));
+
+    file.seekg(0u, std::ios::beg);
+    file.read(std::bit_cast<gl::char_t*>(buffer.data()), size);
+
+    return buffer;
+}
---
+++ run/source.cpp
@@ -0,0 +1,8 @@
+void triangle_example();
+void texture_example();
+
+auto main() -> int
+{
+    triangle_example();
+    texture_example();
+}
