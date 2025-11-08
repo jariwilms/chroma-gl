07a5752f80c658d13910293c08040f4dff69b7ed
Jari <jariwilms@outlook.com>
2025-11-08 12:15:47 +0100

    simpler logic

--- run/examples/load_file.hpp
+++ run/examples/load_file.hpp
@@ -3,25 +3,14 @@
 import std;
 import chroma_gl;
 
-static auto load_file          (std::filesystem::path const& filepath) -> std::vector<gl::byte_t>
+static auto load_file(std::filesystem::path const& filepath) -> std::vector<gl::byte_t>
 {
-    auto file = std::ifstream{ filepath, std::ios::binary | std::ios::ate };
-    if (!file) throw std::runtime_error{ "failed to open file: " + filepath.string() };
+    auto file   = std::ifstream{ filepath, std::ios::binary | std::ios::ate };
+    if (!file) throw std::runtime_error{ "failed to open file" };
 
-    auto const size = file.tellg();
-    auto       buffer = std::vector<gl::byte_t>(static_cast<gl::size_t>(size));
-
-    file.seekg(0u, std::ios::beg);
-    file.read(std::bit_cast<gl::char_t*>(buffer.data()), size);
-
+    auto buffer = std::vector<gl::byte_t>(file.tellg());
+    file.seekg(0u);
+    file.read (reinterpret_cast<gl::char_t*>(buffer.data()), buffer.size());
+    
     return buffer;
-}
-static auto load_file_to_string(std::filesystem::path const& filepath) -> std::string
-{
-    auto file   = std::ifstream{ filepath };
-    auto buffer = std::ostringstream{};
-    if (!file) throw std::runtime_error("failed to open file: " + filepath.string());
-
-    buffer << file.rdbuf();
-    return buffer.str();
 }
